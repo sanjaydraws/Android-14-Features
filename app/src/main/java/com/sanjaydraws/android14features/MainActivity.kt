5d456002481d8e722f65cd1595a42706e61e972f
sanjaydraws <sprajapat8331@gmail.com>
2024-02-02 16:30:55 +0530

    initial project setup

---
+++ app/src/main/java/com/sanjaydraws/android14features/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sanjaydraws.android14features
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
